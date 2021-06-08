8adf2d0e437092abb21206f6d5c0c2fff7bbe69f
Nick Meyer <nmeyer5435@gmail.com>
2021-06-08 18:49:04 -0500

    "Autocommit at 2021-06-08 18:48:43"

--- keyboards/lck75/keymaps/nick5435/config.h
+++ keyboards/lck75/keymaps/nick5435/config.h
@@ -1,7 +1,8 @@
 #pragma once
-#define DEBOUNCING_DELAY 10
+#define DEBOUNCING_DELAY 8
 #define FORCE_NKRO
 
 /* Change USB Polling Rate to 1000hz and a larger keys per scan for elite gaming*/
-#define USB_POLLING_INTERVAL_MS 1
-#define QMK_KEYS_PER_SCAN 12
+// #define USB_POLLING_INTERVAL_MS 5
+// #define QMK_KEYS_PER_SCAN 4
+// #define NO_ACTION_TAPPING
